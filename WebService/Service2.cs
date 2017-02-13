4aa2166731f958481eb7b0ac929e414a3134b12a
lok <lok.ng@hdcircles.com>
2017-02-13 15:37:47 +0800

    Add IService2

---
+++ WebService/Service2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService
+{
+    public class Service2 : IService2
+    {
+        public string GetString()
+        {
+            return "abcded";
+        }
+    }
+}
